f37695792ab2e255d2d28516188428687fe262fc
Fabio Baptista <fabio.bap88@gmail.com>
2020-06-24 22:28:12 -0300

    First commit

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div v-if="loading">Data is loading...</div>
+    <div v-else>
+      <div class="row mb-4" v-for="row in rows" :key="'row'+row">
+        <div
+          class="col d-flex align-items-stretch"
+          v-for="(bookable, column) in bookablesInRow(row)"
+          :key="'row'+row+column"
+        >
+          <bookable-list-item v-bind="bookable"></bookable-list-item>
+        </div>
+        <div class="col" v-for="p in placeholdsInRow(row)" :key="'placeholers'+row+p"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+
+export default {
+  components: {
+    BookableListItem
+  },
+  data() {
+    return {
+      bookables: null,
+      loading: false,
+      columns: 3
+    };
+  },
+  computed: {
+    rows() {
+      return this.bookables === null
+        ? 0
+        : Math.ceil(this.bookables.length / this.columns);
+    }
+  },
+  methods: {
+    bookablesInRow(row) {
+      return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+    },
+    placeholdsInRow(row) {
+      return this.columns - this.bookablesInRow(row).length;
+    }
+  },
+  created() {
+    this.loading = true;
+
+    const p = new Promise((resolve, reject) => {
+      console.log(resolve);
+      console.log(reject);
+      setTimeout(() => resolve("Hello"), 3000);
+    })
+      .then(result => console.log(`Success ${result}`))
+      .catch(result => console.log(`Error ${result}`));
+    console.log(p);
+
+    const request = axios.get("/api/bookables").then(resp => {
+      this.bookables = resp.data;
+      this.bookables.push({ title: "x", description: "x" });
+      this.loading = false;
+    });
+    console.log(request);
+  }
+};
+</script>
